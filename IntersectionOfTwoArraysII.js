e86cfa782d18c92e387554c9de777b0bd9530c01
Marc Abillama <mabillama@gmail.com>
2022-02-16 18:37:00 +0100

    Solution for the problem Intersection of two arrays II

--- IntersectionOfTwoArraysII.js
+++ IntersectionOfTwoArraysII.js
@@ -1,5 +1,7 @@
 /*
 
+https://leetcode.com/explore/interview/card/top-interview-questions-easy/92/array/674/
+
 Given two integer arrays nums1 and nums2, return an array of their intersection. Each element in the result must appear as many times as it shows in both arrays and you may return the result in any order.
 
 Example 1: ****************************
@@ -26,4 +28,39 @@ What if elements of nums2 are stored on disk, and the memory is limited such tha
  * @param {number[]} nums2
  * @return {number[]}
  */
-var intersect = function (nums1, nums2) {};
+var intersect = function (nums1, nums2) {
+  let results = [];
+
+  // Create a map with the elements present on the first array
+  let mappedSet = new Map();
+  nums1.forEach((el) => {
+    !mappedSet.has(el)
+      ? mappedSet.set(el, 1)
+      : mappedSet.set(el, mappedSet.get(el) + 1);
+  });
+
+  // Check the other array against the map and create and populate the results array to be returned afterwards
+
+  nums2.forEach((el) => {
+    if (mappedSet.has(el) && mappedSet.get(el) > 0) {
+      results.push(el);
+      mappedSet.set(el, mappedSet.get(el) - 1);
+    }
+  });
+
+  return results;
+};
+
+nums1 = [4, 9, 5];
+nums2 = [9, 4, 9, 8, 4];
+console.log(intersect(nums1, nums2)); // Output: [4,9]
+nums1 = [1, 2, 2, 1];
+nums2 = [2, 2];
+console.log(intersect(nums1, nums2)); // Output: [2,2]
+
+/*
+Leetcode submission details
+https://leetcode.com/submissions/detail/642708818/?from=explore&item_id=674
+
+
+*/
